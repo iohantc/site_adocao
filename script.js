722f90b5ac2d463af8e48d411e59abe6b3dde9e2
iohantc <iohantcabral@gmail.com>
2023-06-08 12:28:52 -0300

    script de validação

--- script.js
+++ script.js
@@ -1,8 +1,24 @@
 // -----------------------Script Petshop ---------------------
 
 var quant = 0
-function aviso_sacola(){
+function aviso_sacola() {
     alert("Item adicionado à sacola")
 }
 
+function valida_email() {
 
+    emailregex = /^[\w-.]+@([\w-]+.)+[\w-]{2,4}$/g;
+    telefoneregex = /([A-Z])\w+/g;
+
+
+}
+
+function validaNome() {
+    var nome = document.getElementById('nome').value
+    if (nome.indexOf(" ") > -1) {
+        alert('certo')
+    }
+    else {
+        return false
+    }
+}

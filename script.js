66564365e727be5635575be8236fb522cf0293c9
iohantc <iohantcabral@gmail.com>
2023-06-08 14:11:21 -0300

    validação do formulario corrigida

--- script.js
+++ script.js
@@ -5,20 +5,40 @@ function aviso_sacola() {
     alert("Item adicionado à sacola")
 }
 
-function valida_email() {
+// ----------------------Script Validação do formulário-------------------------
 
-    emailregex = /^[\w-.]+@([\w-]+.)+[\w-]{2,4}$/g;
-    telefoneregex = /([A-Z])\w+/g;
+function validaTel(){
+    var telefone = document.getElementById('telefone').value
+    if(telefone.length == 11){
+        telefone = parseInt(telefone)
+        if(telefone > 10000000000){
+            alert('Enviado!')
+        }
+        else{
+            alert('Telefone inválido.\nInsira um telefone válido.')
+        }
+    }
+    else{
+        alert('Telefone inválido.\nInsira um telefone válido.')
+    }
+}
 
-
+function validaEmail(){
+    var email = document.getElementById('email').value
+        if((email.indexOf('@') > -1) && email.indexOf('.') > -1 ){ //verifica se o email tem o @
+            validaTel() //Chama a função vadaTel()
+        } 
+        else{
+            alert('Esse e-mail não existe!\nDigite um e-mail válido.')
+        }
 }
 
 function validaNome() {
-    var nome = document.getElementById('nome').value
-    if (nome.indexOf(" ") > -1) {
-        alert('certo')
+    var nome = document.getElementById('nome').value 
+    if ((nome.length > 5) && nome.indexOf(" ") > -1) {
+        validaEmail()
     }
     else {
-        return false
+        alert('Nome inválido!\nInsira seu nome completo.')
     }
 }
